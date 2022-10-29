a39a4a5ca04cf8f3f723c70b9720057500a3e848
itotakehiro4 <itotakehiro4@gmail.com>
2022-10-29 14:11:08 +0900

    update

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -22,13 +22,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-0ii@(%a&+$$r4r*4#if)+dc%y9)@h!v+=n-=hq*s)6pbtpj1(t"
+# SECRET_KEY = "django-insecure-0ii@(%a&+$$r4r*4#if)+dc%y9)@h!v+=n-=hq*s)6pbtpj1(t"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "3.114.160.155", "[::1]"]
-
+# ALLOWED_HOSTS = ["localhost", "3.114.160.155", "[::1]"]
+ALLOWED_HOSTS = []
 
 # Application definition
 
