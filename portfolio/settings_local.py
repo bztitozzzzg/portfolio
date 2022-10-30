69d10698b944b2be010cc50465e1ecef0b2f9a92
itotakehiro4 <itotakehiro4@gmail.com>
2022-10-31 00:01:38 +0900

    update settings

--- portfolio/settings_local.py
+++ portfolio/settings_local.py
@@ -4,7 +4,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = "y5f2(_*5$sz!@z08*0v#i5fc36x@_7*6=7s@_wedxgxacjud0("
 
-DEBUG = True
+# DEBUG = True
 
 # 本番環境用メール設定
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
