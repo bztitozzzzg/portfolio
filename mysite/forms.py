9e78981b1e48c574e73507b3a289c9ed726e5be4
itotakehiro4 <itotakehiro4@gmail.com>
2022-10-31 13:29:03 +0900

    update forms

--- mysite/forms.py
+++ mysite/forms.py
@@ -1,7 +1,4 @@
 from django import forms
-from django.utils.translation import gettext_lazy as _
-from snowpenguin.django.recaptcha2.fields import ReCaptchaField
-from snowpenguin.django.recaptcha2.widgets import ReCaptchaWidget
 
 
 class ContactForm(forms.Form):
@@ -10,4 +7,3 @@ class ContactForm(forms.Form):
     name = forms.CharField(max_length=100, label="名前")
     email = forms.EmailField(max_length=100, label="メールアドレス")
     message = forms.CharField(label="メッセージ", widget=forms.Textarea())
-    captcha = ReCaptchaField(widget=ReCaptchaWidget())
