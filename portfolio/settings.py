241d0874de787922a2e8e6b255c18edd6f8413ae
itotakehiro4 <itotakehiro4@gmail.com>
2022-11-02 13:57:26 +0900

    update

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "mysite",
     "widget_tweaks",
-    # "snowpenguin.django.recaptcha2",
 ]
 
 MIDDLEWARE = [
@@ -140,6 +139,32 @@ try:
 except ImportError:
     pass
 
+# Heroku settings
+
+# SECRET_KEYの設定
+SECRET_KEY = os.environ['SECRET_KEY']
+
+# staticの設定
+import os
+import django_heroku
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Static files (CSS, JavaScript, Images)
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_URL = "/mysite/static/"
+
+# Extra places for collectstatic to find static files.
+# STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+
+MIDDLEWARE += [
+    "whitenoise.middleware.WhiteNoiseMiddleware",
+]
+
+# HerokuのConfigを読み込み
+django_heroku.settings(locals())
+
+"""
 if not DEBUG:
     # Heroku settings
 
@@ -165,3 +190,4 @@ if not DEBUG:
 
     # HerokuのConfigを読み込み
     django_heroku.settings(locals())
+"""
