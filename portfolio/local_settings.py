aa9c9741f4558185c96ac8a7d872f22d30ab5c58
itotakehiro4 <itotakehiro4@gmail.com>
2022-11-02 13:07:15 +0900

    ignore

--- portfolio/local_settings.py
+++
@@ -1,8 +0,0 @@
-import os
-
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-# SECRTE KEY
-SECRET_KEY = '4#%ko@7ukrh$m309u+03%4g)g8emrb8gjq1j!)id&3j&k13-8('
-
-DEBUG = True
