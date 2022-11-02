f13e5bd181dbc5bf67020f70a96734b2ef52de42
itotakehiro4 <itotakehiro4@gmail.com>
2022-11-02 14:44:22 +0900

    update

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
-from .local_settings import *
+from .local_settings import SECRET_KEY
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -141,7 +141,8 @@ except ImportError:
     pass
 
 # SECRET_KEYの設定
-SECRET_KEY = os.environ['SECRET_KEY']
+#SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = SECRET_KEY
 
 if not DEBUG:
     # Heroku settings
