ca9a57b9f9fd6c72067de2f4ec1fa14d19a82559
itotakehiro4 <itotakehiro4@gmail.com>
2022-11-02 17:16:55 +0900

    update settings

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -130,9 +130,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 # 開発環境用メール設定
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
-# Falseに変更
-DEBUG = False
-
 try:
     # 存在する場合、ローカルの設定読み込み
     from .local_settings import *
